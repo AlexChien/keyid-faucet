667f7e30c3642de36e73e9a0d608b5e008a5c337
Daniel Brockman <daniel@brockman.se>
2014-09-16 17:20:30 +0200

    1 file changed (+1/-1)

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 var app = angular.module('index', [])
 
 app.controller('IndexController', function ($scope, $http) {
-  $http('/profile.json').then(function (profile) {
+  $http.get('/profile.json').then(function (profile) {
     $scope.profile = profile
   })
 })
