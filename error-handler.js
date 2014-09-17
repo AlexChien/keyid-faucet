127964a319dda8992f0d8ea1ca6ddae1f1f59d30
Daniel Brockman <daniel@brockman.se>
2014-09-17 23:50:02 +0200

    1 file changed (+1/-1)

--- error-handler.js
+++ error-handler.js
@@ -1,4 +1,4 @@
-module.exports = function(error, request, response, next) {
+module.exports = function (error, request, response, next) {
   console.error(err.stack)
   response.status(500).send({
     error: 'Internal server error: ' + error.message
