34206da307c359ee1a35dfa8f1c3979a461a79b7
Daniel Brockman <daniel@brockman.se>
2014-09-17 19:17:33 +0200

    3 files changed (+32/-20)

--- public/index.js
+++ public/index.js
@@ -1,7 +1,14 @@
 var app = angular.module('index', [])
 
 app.controller('IndexController', function ($scope, $http) {
-  $http.get('/user.json').success(function (data) {
+  $http.get('/user').success(function (data) {
     $scope.user = data.user
+    $scope.register = function (key, name) {
+      $http.post('/register', {
+        key: key, name: name
+      }).success(function (data) {
+        $scope.result = data
+      })
+    }
   })
 })
--- server.js
+++ server.js
@@ -2,16 +2,16 @@ var app = require('express')()
 
 app.use(require('express').static(__dirname + '/public'))
 app.use(require('morgan')('dev'))
-app.use(require('body-parser').urlencoded({ extended: true }))
+app.use(require('body-parser').json())
 require('./passport.js')(app)
 app.use(require('./parse-user.js'))
 
-app.get('/user.json', function (request, response) {
+app.get('/user', function (request, response) {
   response.json({ user: request.parsed_user })
 })
 
 // XXX: For debugging
-app.get('/user.raw.json', function (request, response) {
+app.get('/user/raw', function (request, response) {
   response.json(request.user)
 })
 
