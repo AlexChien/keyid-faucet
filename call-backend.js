d3c4383f337002b44d2a3b703d62b2dcd18eb3e9
Daniel Brockman <daniel@brockman.se>
2014-09-17 19:32:48 +0200

    1 file changed (+1/-1)

--- call-backend.js
+++ call-backend.js
@@ -2,7 +2,7 @@ var BACKEND_SECRET = require('./getenv.js')('BACKEND_SECRET')
 var BACKEND_URL = require('./getenv.js')('BACKEND_URL')
 
 module.exports = function (request, response) {
-  console.dir('request.body: %j', request.body)
+  console.log('request.body: %j', request.body)
   require('request').post({
     url: BACKEND_URL, json: request.body, headers: {
       authorization: require('hawk').client.header(BACKEND_URL, 'POST', {
