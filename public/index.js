3c3e6c21f1c26e6905cccbaeedc6a3a674f22fbf
Daniel Brockman <daniel@brockman.se>
2014-09-17 19:44:17 +0200

    2 files changed (+6/-7)

--- public/index.js
+++ public/index.js
@@ -3,10 +3,9 @@ var app = angular.module('index', [])
 app.controller('IndexController', function ($scope, $http) {
   $http.get('/user').success(function (data) {
     $scope.user = data.user
-    $scope.register = function (key, name) {
-      $http({
-        method: 'POST', url: '/register-account',
-        data: { key: key, name: name }
+    $scope.register_account = function () {
+      $http.post('/register-account', {
+        key: $scope.key, name: $scope.name
       }).success(function (data) {
         $scope.result = data
       })
